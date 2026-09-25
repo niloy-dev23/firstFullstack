378fe9521848538129bca6344236f3ebb88e8e52
Nafis Sazzad Niloy <niloynafis.dev@gmail.com>
2026-09-25 07:51:15 +0600

    cloudinaryDone

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 let databaseURL = process.env.DB_URL
 const dbConfig = ()=>{
-    mongoose.connect(databaseURL)
+    mongoose.connect(databaseURL).then(()=>{
+        console.log('Database Connected')
+    })
 }
 
 module.exports = dbConfig
--- server/routes/api/multerRoute.js
+++ server/routes/api/multerRoute.js
@@ -2,18 +2,33 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const registration = require('../../controller/authController')
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
-const uploadStorage = multer.diskStorage({
-    destination: function (req, file, cb){
-        cb(null, './uploads')
-    },
-    filename: function (req, file, cb){
-        const uniqueSuffix = 'img- ' + file.originalname
-        cb(null, file.fieldname + uniqueSuffix)
-    }
-})
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET
+});
 
-const upload = multer({storage: uploadStorage})
+const cloudStorage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'firstCloudinary',
+    allowedFormat: ['png', 'jpeg', 'jpg']
+  },
+});
+// const uploadStorage = multer.diskStorage({
+//     destination: function (req, file, cb){
+//         cb(null, './uploads')
+//     },
+//     filename: function (req, file, cb){
+//         const uniqueSuffix = 'img- ' + file.originalname
+//         cb(null, file.fieldname + uniqueSuffix)
+//     }
+// })
+
+const upload = multer({storage: cloudStorage})
 router.post('/registration', upload.single('picture'), registration)
 
 module.exports = router
